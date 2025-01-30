821c8e4f88096ffb6f3aa0b0943ef4a1934f8605
Mojes Kamlapure <mojeskamlapure@ymail.com>
2025-01-31 00:29:29 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello buddy 123!";
+        return "Hello buddy 123! I am back";
     }   
 }
