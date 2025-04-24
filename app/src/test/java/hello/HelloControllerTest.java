ca70aa6c93f2857ff3912c7db07a9d707345c23a
mojeskamlapure <mojeskamlapure@ymail.com>
2025-04-24 17:38:12 +0530

    added junit

--- app/src/test/java/hello/HelloControllerTest.java
+++ app/src/test/java/hello/HelloControllerTest.java
@@ -1,11 +1,10 @@
 package hello;
-/*
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
@@ -26,8 +25,8 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello SocGen !")));
+                .andExpect(content().string(equalTo("This is awesome..! I am updated now Ok..!")));
     }
     
 }
-*/
+
