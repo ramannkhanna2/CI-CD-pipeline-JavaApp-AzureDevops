fb88924c0eca96490c0879bda43e030ea30e6c0a
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2026-02-21 20:21:15 +0530

    Change greeting message from Friday to Saturday

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello all today is Friday !";
+        return "Hello all today is Saturday !";
     }   
 }
